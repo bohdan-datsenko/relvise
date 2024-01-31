aecee6f890202079042a6410f24e023e30b445fd
Bohdan Datsenko <reborn.muffin@gmail.com>
2024-01-31 02:37:04 +0200

    fix of burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-let menu = document.getElementById("burgerMenuBackdrop");
+let backdrop = document.getElementById("burgerMenuBackdrop");
+let menu = document.getElementById("burgerMenu");
 let body = document.body;
 let html = document.documentElement;
 
-menu.addEventListener("click", () => {
+backdrop.addEventListener("click", () => {
     menu.classList.toggle("active");
     body.classList.toggle("lock");
     html.classList.toggle("lock");
